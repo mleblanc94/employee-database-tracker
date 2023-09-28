6404e53aaad2dc538849a46c34ab0b4cbb0fa8e0
Michael Leblanc <mleblanc1@werfen.com>
2023-09-28 16:42:54 -0400

    added actions for the user as well as modified the schema to auto increment for ids

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,19 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   dep_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary decimal NOT NULL,
   department_id INT
 );
 
 CREATE TABLE employee(
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
